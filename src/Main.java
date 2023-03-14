c6f6ce16972fc77a91a32d25507532c3d465a283
Geternitier <211250036@smail.nju.edu.cn>
2023-03-14 17:48:19 +0800

    listener written

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main
 {    
@@ -14,9 +16,27 @@ public class Main
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
 
+        // ErrorListener
         SysYErrorListener listener = new SysYErrorListener();
         sysYLexer.removeErrorListeners();
         sysYLexer.addErrorListener(listener);
+        List<? extends Token> tokens = sysYLexer.getAllTokens();
+        if(listener.listen()){
+            return;
+        }
+
+        String[] rules = sysYLexer.getRuleNames();
+        for(Token token: tokens){
+            String text = token.getText();
+            if(text.startsWith("0x")||text.startsWith("0X")){
+                text = String.valueOf(Integer.parseInt(text.substring(2), 16));
+            }
+            if(text.startsWith("0")){
+                text = String.valueOf(Integer.parseInt(text.substring(1), 8));
+            }
+            System.out.println(rules[token.getType()-1]+' '+text+" at Line "+token.getLine());
+        }
+
     }
 
 }
--- src/SysYErrorListener.java
+++ src/SysYErrorListener.java
@@ -1,5 +1,17 @@
 import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 
 public class SysYErrorListener extends BaseErrorListener {
-    
+    private boolean isError = false;
+    public boolean listen(){
+        return isError;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object o, int lineNum, int posInLine, String msg, RecognitionException e){
+        isError = true;
+        System.err.println("Error type A at Line " + lineNum + ": " + msg + '.');
+    }
+
 }
